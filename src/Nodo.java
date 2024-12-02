818b831ea240edbcd5520f17c240796ffbe12833
martinizin <mar45pillacela@gmail.com>
2024-12-02 10:45:56 -0500

    Creacion de clases

---
+++ src/Nodo.java
@@ -0,0 +1,12 @@
+public class Nodo {
+    int dato;
+    Nodo siguiente;
+    Nodo anterior;
+
+    // Constructor
+    public Nodo(int dato) {
+        this.dato = dato;
+        this.siguiente = null;
+        this.anterior = null;
+    }
+}
